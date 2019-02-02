f0d0963e185ff61e9ee5eab7a8109f4ee8ffd539
JENEVER1 <46226826+JENEVER1@users.noreply.github.com>
2019-02-03 00:41:14 +0700

    Add files via upload

---
+++ 5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Author By : D4RKS1D3 SQUAD
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+toilet -f future DUNIA JAMAN NOW |lolcat
+
+echo ""
+echo $blue"______________________________"
+echo $blue"Create       : root@M3e.X"
+echo $blue"My Team      : D4RKS1D3 SQUAD & Attack Of Cyber"
+echo $blue"Contact      : 0895-3975-17177 atau +1(518)299-8105"
+echo $blue"______________________________"
+echo ""
+echo $blue"FLAG DarksideSquad(Sayur Kol Ikan Anjing)"
